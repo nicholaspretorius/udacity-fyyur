37f32308e6bfb870ab39fa65de9fb080c969c77a
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-11-14 15:09:46 +0200

    feat: Update artist search

--- app.py
+++ app.py
@@ -327,14 +327,18 @@ def search_artists():
     # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
     # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
     # search for "band" should return "The Wild Sax Band".
-    response = {
-        "count": 1,
-        "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
-    }
+    # response = {
+    #     "count": 1,
+    #     "data": [{
+    #         "id": 4,
+    #         "name": "Guns N Petals",
+    #         "num_upcoming_shows": 0,
+    #     }]
+    # }
+    term = request.form.get('search_term')
+    search = "%{}%".format(term.lower())
+    res = Artist.query.filter(Artist.name.ilike(search)).all()
+    response = {'count': len(res), 'data': res}
     return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 
